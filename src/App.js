cfdfc41d3e616868c83c8541e0472df6a77fc765
Trevor Fairbank <Trevorfairbank92@gmail.com>
2020-03-03 15:18:49 -0800

    got rid of alerts, alerts are lame

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Main from "./components/Main";
@@ -7,15 +7,14 @@ import Footer from "./components/Footer";
 import images from "./images.json";
 
 class App extends React.Component {
-  
-  state ={
+  state = {
     images,
     score: 0,
     topscore: 0,
     clickedarray: []
-  }
+  };
 
-   randomOrder = () => {
+  randomOrder = () => {
     return this.state.images.sort(() => Math.random() - 0.5);
   };
 
@@ -23,46 +22,36 @@ class App extends React.Component {
     const currentLegend = event.target.id;
 
     const legendClicked = this.state.clickedarray.indexOf(currentLegend) > -1;
-    if (legendClicked){
-      this.setState({
-        images: this.randomOrder(),
-        clickedarray:[],
-        score:0,
-      })
-    alert("Sorry, try Again.")
-    }
-    else {
-      this.setState({
-        images: this.randomOrder(),
-        clickedarray:this.state.clickedarray.concat(currentLegend),
-        score:this.state.score + 1
-      })
-    }
 
-    //update topscore
-    if (this.state.score > this.state.topscore){
-      this.setState({ topscore: this.state.score});
-    }
-    if (this.state.score === 11){
-      alert("YOU WON!!! GREAT JOB!!!! NOW DO IT AGAIN.")
+    if (legendClicked) {
       this.setState({
         images: this.randomOrder(),
-        clickedarray:[],
-        score:0,
-      })
+        clickedarray: [],
+        score: 0
+      });
+    } else {
+      this.setState({
+        images: this.randomOrder(),
+        clickedarray: this.state.clickedarray.concat(currentLegend),
+        score: this.state.score + 1
+      });
+
+      //update topscore
+      if (this.state.score >= this.state.topscore) {
+        this.setState({ topscore: this.state.score + 1 });
+      }
     }
-    
-  }
-  
+  };
+
   render() {
-  return (
-    <div>
-    <Navbar score={this.state.score} topscore={this.state.topscore}/>
-    <Header />
-    <Main handleimageclick={this.handleImageClick} />
-    <Footer />
-    </div>
-  );
+    return (
+      <div>
+        <Navbar score={this.state.score} topscore={this.state.topscore} />
+        <Header />
+        <Main handleimageclick={this.handleImageClick} />
+        <Footer />
+      </div>
+    );
   }
 }
 
